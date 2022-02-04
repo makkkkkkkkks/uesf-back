915785394883da2ee55a038772ff46e3e22f8c16
makkkkkkkkksInmost <51357230+makkkkkkkkks@users.noreply.github.com>
2022-02-04 20:12:29 +0200

    Initial commit

---
+++ src/main/java/org/ua/uesf/uesfback/UesfBackApplication.java
@@ -0,0 +1,13 @@
+package org.ua.uesf.uesfback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UesfBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UesfBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/ua/uesf/uesfback/UesfBackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ua.uesf.uesfback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UesfBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
